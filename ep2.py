60df5a0afb62b3f74ceee655fc88761eafe7b2cb
joaopcm3 <joaopcm3@al.insper.edu.br>
2024-04-30 21:07:33 -0300

    commit 4

--- ep2.py
+++ ep2.py
@@ -94,7 +94,84 @@ def foi_derrotado(matriz):
     
     return True
 
+##############################
+######## INFORMAÇÔES #########
+##############################
+
+# Black: \u001b[30m
+# Red: \u001b[31m
+# Green: \u001b[32m
+# Yellow: \u001b[33m
+# Blue: \u001b[34m
+# Magenta: \u001b[35m
+# Cyan: \u001b[36m
+# White: \u001b[37m
+# Reset: \u001b[0m
+
+CONFIGURACAO = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
+
+PAISES =  {
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }, 
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    },
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    },
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    },
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }
+}
+
+CORES = {
+    'reset': '\u001b[0m',
+    'red': '\u001b[31m',
+    'black': '\u001b[30m',
+    'green': '\u001b[32m',
+    'yellow': '\u001b[33m',
+    'blue': '\u001b[34m',
+    'magenta': '\u001b[35m',
+    'cyan': '\u001b[36m',
+    'white': '\u001b[37m'
+}
+
+alfabeto_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+alfabeto = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
+
+##############################
+
 ############
 ### JOGO ###
 ############
-
